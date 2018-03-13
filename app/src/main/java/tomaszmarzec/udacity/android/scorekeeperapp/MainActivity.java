970010a4af222e932e45db9f3d2001861b8ed993
Tomasz Marzec <tomaszmarzec95@gmail.com>
2018-03-13 20:21:16 +0100

    Imported Glide library, added background (rendered by Glide), changed strings.xml.

--- app/src/main/java/tomaszmarzec/udacity/android/scorekeeperapp/MainActivity.java
+++ app/src/main/java/tomaszmarzec/udacity/android/scorekeeperapp/MainActivity.java
@@ -1,7 +1,17 @@
 package tomaszmarzec.udacity.android.scorekeeperapp;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +21,33 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ConstraintLayout rootView = findViewById(R.id.rootView);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+        renderImages(rootView, height, width);
     }
+
+    private void renderImages(final ConstraintLayout rootView, int height, int width)
+    {
+        //Code based on StackOverflow topic: https://stackoverflow.com/questions/33971626/set-background-image-to-relative-layout-using-glide-in-android/38025862
+        // Answer by Chintan Desai
+        Glide.with(this).load(R.drawable.field).asBitmap().into(new SimpleTarget<Bitmap>(width, height)
+        {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation)
+            {
+                Drawable drawable = new BitmapDrawable(resource);
+                rootView.setBackground(drawable);
+            }
+        });
+    }
+
 }
+
+
+
