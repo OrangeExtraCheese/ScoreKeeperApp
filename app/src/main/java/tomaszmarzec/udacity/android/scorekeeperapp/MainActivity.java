2889e9654800e2a524b0c319cd6eb09564e5d1dd
Tomasz Marzec <tomaszmarzec95@gmail.com>
2018-03-18 16:04:35 +0100

    First fully functional version.

--- app/src/main/java/tomaszmarzec/udacity/android/scorekeeperapp/MainActivity.java
+++ app/src/main/java/tomaszmarzec/udacity/android/scorekeeperapp/MainActivity.java
@@ -20,23 +20,22 @@ import com.bumptech.glide.request.target.SimpleTarget;
 
 public class MainActivity extends AppCompatActivity
 {
-/*    What is a good solution/practise here? In this one I'm declaring global object variables, referencing buttons and TextViews. These globals are then accesed from methods, like aFoulsIncrease().
-     They are initialized in onCreate() method.
+/*    What is a good solution/practise here? In this one I'm declaring global object variables, referencing Buttons and TextViews. They are initialized in onCreate() method.
+      These memeber variables are then accesed from methods, like aFoulsIncrease().
+
      The other solution is to use this variables as local ones, inside methods, but then there would be many methods for displaying changes, for example, instead of single
     public void display (int numb, TextView textView)
     {
         textView.setText(""+numb);
     }
-    there would be many:
+    there would be many like:
     public void displayTeamAScore (int numb)
     {
         TextView txtTeamAScore = findViewById(R.id.team_a_score);
         txtTeamAScore.setText(""+numb);
     }
 
-    Also, variables referencing buttons and TextViews will be created many times, for each method call, if I'm correct. When they are global they are declared and initialized once.
-    On the other hand, I ecountered advices to avoid global variables.
-
+    On the other hand using member variables is discouraged because of risk of memory leaks. What is a good strategy to handle this matter?
     */
 
     Button btnTeamASnitch;
@@ -45,11 +44,15 @@ public class MainActivity extends AppCompatActivity
     Button btnTeamBGoal;
     Button btnTeamAFoul;
     Button btnTeamBFoul;
+    Button btnReset;
 
     TextView txtTeamAScore;
     TextView txtTeamBScore;
     TextView txtTeamAFouls;
     TextView txtTeamBFouls;
+    TextView txtTeamAName;
+    TextView txtTeamBName;
+    TextView txtWinner;
 
     int aScore = 0;
     int bScore = 0;
@@ -66,6 +69,9 @@ public class MainActivity extends AppCompatActivity
         txtTeamBScore =  findViewById(R.id.team_b_score);
         txtTeamAFouls =  findViewById(R.id.team_a_fouls);
         txtTeamBFouls =  findViewById(R.id.team_b_fouls);
+        txtTeamAName=    findViewById(R.id.team_a_name);
+        txtTeamBName=    findViewById(R.id.team_b_name);
+        txtWinner =      findViewById(R.id.winner);
 
         btnTeamAGoal = findViewById(R.id.button_goalA);
         btnTeamBGoal = findViewById(R.id.button_goalB);
@@ -73,6 +79,7 @@ public class MainActivity extends AppCompatActivity
         btnTeamBFoul = findViewById(R.id.button_foulB);
         btnTeamASnitch = findViewById(R.id.snitchA);
         btnTeamBSnitch = findViewById(R.id.snitchB);
+        btnReset = findViewById(R.id.button_reset);
 
         renderImages();
 
@@ -89,7 +96,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this, R.style.AlertDialogTheme);
                 mBuilder.setTitle(R.string.dialog_title);
                 mBuilder.setMessage(R.string.dialog_msg);
                 if(whichTeam == 0)
@@ -183,6 +190,7 @@ public class MainActivity extends AppCompatActivity
     {
         aScore+=150;
         display(aScore, txtTeamAScore);
+        displayWinner();
         buttonsOn(false);
     }
 
@@ -190,9 +198,11 @@ public class MainActivity extends AppCompatActivity
     {
         bScore+=150;
         display(bScore, txtTeamBScore);
+        displayWinner();
         buttonsOn(false);
     }
 
+    //This method turns button active or inactive. It is intended for all buttons except reset to be inactive when match is finished.
     private void buttonsOn(boolean flag)
     {
         btnTeamASnitch.setEnabled(flag);
@@ -201,6 +211,16 @@ public class MainActivity extends AppCompatActivity
         btnTeamBGoal.setEnabled(flag);
         btnTeamAFoul.setEnabled(flag);
         btnTeamBFoul.setEnabled(flag);
+    }
+
+    private void displayWinner()
+    {
+        if (aScore > bScore)
+            txtWinner.setText(txtTeamAName.getText()+ getResources().getString( R.string.victory_text));
+        else
+            txtWinner.setText(txtTeamBName.getText()+ getResources().getString( R.string.victory_text));
+
+        btnReset.setText(R.string.new_match);
     }
 
     public void reset(View view)
@@ -214,7 +234,11 @@ public class MainActivity extends AppCompatActivity
         bFouls=0;
         display(bFouls, txtTeamBFouls);
 
+        txtTeamAName.setText("");
+        txtTeamBName.setText("");
+
         buttonsOn(true);
+        btnReset.setText(R.string.btn_reset);
     }
 
 }
